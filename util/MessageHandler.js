54e496e0cbcae7e1af00a6d201e8746903bd4222
Omak2r3a <86030102+kerollosy@users.noreply.github.com>
2024-05-14 15:59:09 +0300

    Gateway will send a `t` only if it's op code 0 (Dispatch)

--- util/MessageHandler.js
+++ util/MessageHandler.js
@@ -22,16 +22,57 @@ export class MessageHandler {
      */
     handle(message) {
         this.client.emit("debug", message);
-        
+
         let json = JSON.parse(message);
         let op = json.op;
         let t = json.t;
         let data = json.d;
-        this.client.s = json.s;
 
         switch (op) {
-            // 0: TODO: Dispatch
+            // 0: Dispatch
             case 0:
+                this.client.s = json.s;
+                switch (t) {
+                    case PACKETS.MESSAGE_CREATE:
+                        let channel = this.client.channels.get(data.channel_id);
+                        if (channel) {
+                            let msg = channel.messages.set(data.id, new Message(data, this.client));
+                            this.client.emit("message", msg);
+                        } else {
+                            this.client.emit("debug", `Received message for unknown channel ${data.channel_id}`);
+                        }
+                        break;
+
+                    case PACKETS.MESSAGE_DELETE:
+                        channel = this.client.channels.get(data.channel_id);
+                        if (channel) {
+                            let msg = channel.messages.get(data.id);
+                            if (msg) {
+                                channel.messages.delete(data.id);
+                                this.client.emit("messageDelete", msg);
+                            } else {
+                                this.client.emit("debug", `Received delete message for unknown message ${data.id}`);
+                            }
+                        } else {
+                            this.client.emit("debug", `Received delete message for unknown channel ${data.channel_id}`);
+                        }
+                        break;
+
+                    case PACKETS.READY:
+                        this.client.ready = true;
+                        this.client.readyTime = Date.now();
+                        this.client.user = this.client.users.set(data.id, new User(data.user, this.client));
+                        data.guilds.forEach(guild => {
+                            this.client.guilds.set(guild.id, { unavailabe: true });
+                        });
+                        this.client.emit("ready", this.client.user);
+                        this.client.emit("debug", `Cached ${this.client.guilds.size} servers`);
+                        break;
+
+                    case PACKETS.GUILD_CREATE:
+                        this.client.guilds.set(data.id, new Guild(data, this.client));
+                        break;
+                }
                 break;
             // 1: Heartbeat
             case 1:
@@ -44,6 +85,7 @@ export class MessageHandler {
             // 9: TODO: Invalid session
             case 9:
                 console.log("Invalid session, reidentifying");
+                this.client.ws.send(JSON.stringify({ "op": 2, "d": { "token": this.client.token, "intents": this.client.intents } }));
                 break;
             // 10: Hello
             case 10:
@@ -56,47 +98,6 @@ export class MessageHandler {
                 break;
         }
 
-        switch (t) {
-            case PACKETS.MESSAGE_CREATE:
-                let channel = this.client.channels.get(data.channel_id);
-                if (channel) {
-                    let msg = channel.messages.set(data.id, new Message(data, this.client));
-                    this.client.emit("message", msg);
-                } else {
-                    this.client.emit("debug", `Received message for unknown channel ${data.channel_id}`);
-                }
-                break;
-            
-            case PACKETS.MESSAGE_DELETE:
-                channel = this.client.channels.get(data.channel_id);
-                if(channel) {
-                    let msg = channel.messages.get(data.id);
-                    if(msg) {
-                        channel.messages.delete(data.id);
-                        this.client.emit("messageDelete", msg);
-                    } else {
-                        this.client.emit("debug", `Received delete message for unknown message ${data.id}`);
-                    }
-                } else {
-                    this.client.emit("debug", `Received delete message for unknown channel ${data.channel_id}`);
-                }
-                break;
-
-            case PACKETS.READY:
-                this.client.ready = true;
-                this.client.readyTime = Date.now();
-                this.client.user = this.client.users.set(data.id, new User(data.user, this.client));
-                data.guilds.forEach(guild => {
-                    this.client.guilds.set(guild.id, { unavailabe: true });
-                });
-                this.client.emit("ready", this.client.user);
-                this.client.emit("debug", `Cached ${this.client.guilds.size} servers`);
-                break;
-
-            case PACKETS.GUILD_CREATE:
-                this.client.guilds.set(data.id, new Guild(data, this.client));
-                break;
-        }
     }
 }
 
